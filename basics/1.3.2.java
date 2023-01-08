914b1238b1c728d8d39f900b592d81ba3dde0f31
Vedant <vedantcoding31@gmail.com>
2023-01-08 21:43:13 +0530

    Create 1.3.2.java

---
+++ basics/1.3.2.java
@@ -0,0 +1,21 @@
+class test {
+    // max of three IF-ELSE laddr operator [1.3.1]
+    public static void main(String[] args)
+    {
+        int a = 100, b = 200, c = 300;
+
+        if (a > b) {
+            if (a > b) {
+                System.out.println(a + " is greater");
+            } else if (a < c) {
+                System.out.println(c + " is greater");
+            }
+        } else if (b > a) {
+            if (b > c) {
+                System.out.println(b + " is greater");
+            } else if (c > b) {
+                System.out.println(c + " is greater");
+            }
+        }
+    }
+}
