ed30f85b4cd9bcf1e05acf73cafba25cea0f0c1e
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-01-30 20:19:20 +0530

    Create 4.1.java

---
+++ basics/4.1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+//4.1
+class person{
+    int age, salary;
+    String name;
+}
+
+class main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        person p = new person();
+
+        System.out.print("Enter name: ");
+        p.name = s.nextLine();
+        System.out.print("Enter age: ");
+        p.age = s.nextInt();
+        System.out.print("Enter salary: ");
+        p.salary = s.nextInt();
+
+        System.out.println("name is: "+p.name);
+        System.out.println("age is: "+p.age);
+        System.out.println("salary is: "+p.salary);
+    }
+}
