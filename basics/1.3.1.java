f53e9e782e0aae1f7b8aad1699a49170c2ba4297
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-02-08 00:04:31 +0530

    Update 1.3.1.java

--- basics/1.3.1.java
+++ basics/1.3.1.java
@@ -2,6 +2,10 @@ class test {
     // max of three TERNARY operator [1.3.1]
     public static void main(String[] args)
     {
+        
+        System.out.println("Vedant Patel IU2141230208");
+        System.out.println("-------------------------");
+        
         int a = 30, b = 22, c = 13, d;
         d = ((a > b && a > c) ? a : ((b > a && b > c) ? b : c));
 
