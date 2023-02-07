805da8212db51f04abc740f939c0e522f44d9df1
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-02-08 00:07:19 +0530

    Update 1.4.java

--- basics/1.4.java
+++ basics/1.4.java
@@ -2,10 +2,13 @@ class test{
 	//fibonacci [1.4]
 	public static void main(String[] args){
 		int a = 0, b = 1, c;
-
+		
+		System.out.println("Vedant Patel IU2141230208");
+     		System.out.println("-------------------------");
+		
 		for (int i = 0; i < 10; ++i)
 		{
-			System.out.println(a);
+			System.out.print(a + " ");
 			c = a+b;
 			a = b;
 			b = c;
