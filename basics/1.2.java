21e3d92cbcd758f0d8e9b375aa92ce447db0d788
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-02-08 00:01:55 +0530

    Update 1.2.java

--- basics/1.2.java
+++ basics/1.2.java
@@ -1,18 +1,22 @@
-class test{
-	//prime or not [1.2]
-	public static void main(String[] args){
-		int a = 11, temp = 0;
+class main {
+    // prime or not [1.2]
+    public static void main(String[] args)
+    {
+        System.out.println("Vedant Patel IU2141230208");
+        System.out.println("-------------------------");
 
-		for (int i = 1; i < a/2 ; ++i ) {
-			if (a%i == 0) {
-				temp++;
-			}
-		}
+        int a = 11, temp = 0;
 
-		if (temp == 1) {
-			System.out.println("is prime");
-		} else {
-			System.out.println("not prime");
-		}
-	}
+        for (int i = 2; i < a / 2; ++i) {
+            if (a % i == 0) {
+                temp++;
+            }
+        }
+
+        if (temp == 0) {
+            System.out.println(a + " is prime");
+        } else {
+            System.out.println(a + " not prime");
+        }
+    }
 }
