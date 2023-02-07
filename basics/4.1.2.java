d8410f7b6049560c6d8fff5c0eba918b5ede2719
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-02-08 00:26:15 +0530

    Update 4.1.2.java

--- basics/4.1.2.java
+++ basics/4.1.2.java
@@ -1,12 +1,15 @@
 import java.util.*;
 //4.1.2
 class person {
+
     Scanner s = new Scanner(System.in);
     int age, salary;
     String name;
 
     person()
     {
+        System.out.print("\nConstructor starts:\n\n");
+
         System.out.print("Enter name: ");
         name = s.nextLine();
         System.out.print("Enter age: ");
@@ -26,8 +29,13 @@ class person {
 class main {
     public static void main(String[] args)
     {
+        System.out.println("Vedant Patel IU2141230208");
+        System.out.println("-------------------------");
+
         Scanner s = new Scanner(System.in);
         person p = new person();
+        System.out.println("\nCalling the display function.\n");
+
         p.display();
     }
 }
