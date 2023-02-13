f0e1ad6dce49ca1f5872506e74c0a87984059de9
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-02-13 14:28:22 +0530

    Create 5.1.java

---
+++ basics/5.1.java
@@ -0,0 +1,90 @@
+	import java.util.*;
+
+	class Employee{
+		Scanner s = new Scanner(System.in);
+		String name;
+		int emp_numb;
+		char gender;
+
+		void input_data(){
+			System.out.print("Enter name: ");
+			name = s.nextLine();
+
+			System.out.print("Enter gender(M/F): ");
+			gender = s.next().charAt(0);
+
+			while((gender != 'M') && (gender != 'F') ) {
+				System.out.print("please enter gender again: ");
+				gender = s.next().charAt(0);
+			}
+
+			System.out.print("Enter emp_number: ");
+			emp_numb = s.nextInt();
+			
+		}
+
+		void display_data(){
+			System.out.println("------------------------------");
+
+			System.out.println("Employee name: "+name);
+
+			System.out.println("Employee number: "+emp_numb);
+
+			System.out.println("Employee gender: "+gender);
+
+			System.out.println("------------------------------");
+
+			
+		}
+	}
+
+	class SalariedEmployee extends Employee{
+		float salary, hra, da;
+
+		public void allowance(){
+			System.out.println("Enter salary: ");
+			salary = s.nextFloat();
+
+			if(gender == 'F'){
+				hra = ((float) 0.1) * salary;
+			} else {
+				hra = ((float) 0.09) * salary;
+			}
+
+			da = ((float) 0.05) * salary;
+
+			salary = salary + hra + da;
+		}
+
+
+		void increment(){
+			salary = salary + ((float)0.1) * salary ;
+			System.out.println();
+
+		}
+	}
+
+	class main{
+		public static void main(String[] args) {
+
+			System.out.println("\n**************************");
+			System.out.println("Vedant Patel IU2141230208");
+			System.out.println("**************************");
+
+			SalariedEmployee person = new SalariedEmployee();
+
+			person.input_data();
+
+			person.display_data();
+
+			person.allowance();
+
+			person.increment();
+
+			System.out.println("------------------------------");
+			System.out.println("\nGross salary = " + person.salary);
+			System.out.println("------------------------------");
+
+		}
+	}
+
