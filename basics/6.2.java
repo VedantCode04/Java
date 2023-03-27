098f64b53d795e1979cbf65a7fd82344eb4b8681
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-03-27 22:49:32 +0530

    Create 6.2.java

---
+++ basics/6.2.java
@@ -0,0 +1,32 @@
+interface Shape2D {
+    void getArea();
+}
+
+abstract class Shape {
+    abstract void display();
+}
+
+class Circle extends Shape implements Shape2D {
+    private double radius;
+    
+    Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    void display() {
+        System.out.println("Circle with radius " + radius);
+    }
+    
+    public void getArea() {
+        double area =  Math.PI * radius * radius;
+        System.out.println("Area of circle: " + area);
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        circle.display();
+        circle.getArea();
+    }
+}
