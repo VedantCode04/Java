59a40f19dccc2e2516063a04cc14301cd0635003
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-03-27 22:53:45 +0530

    Create 5.2.java

---
+++ basics/5.2.java
@@ -0,0 +1,24 @@
+class A3 {
+    void hello(String s) {
+        System.out.println("A3: Hello From " + s);
+    }
+}
+
+class B3 extends A3 {
+    void hello(String s) {
+        super.hello(s);
+        System.out.println("B3: Hello From " + s);
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        A3 obj1 = new A3();
+        B3 obj2 = new B3();
+        A3 obj3 = new B3();
+
+        obj1.hello("Object 1");
+        obj2.hello("Object 2");
+        obj3.hello("Object 3");
+    }
+}
